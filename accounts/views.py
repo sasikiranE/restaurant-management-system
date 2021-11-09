33b32b60770d549903a887c563a6c1cca08b61f2
Sasi Kiran E <sasikiranessamaraju@gmail.com>
2021-11-09 14:43:50 +0530

    removed unnecessary import statement

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,3 @@
-from django.db.models.query import RawQuerySet
 from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth.models import User
 from django.contrib import auth, messages
