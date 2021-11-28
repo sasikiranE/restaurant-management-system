e4c8b87fc77a9acdcb2442444f81c066d23ae2ea
sasikiran <sasikiranessamaraju@gmail.com>
2021-11-28 10:48:32 +0530

    customized slideradmin teamadmin itemsadmin

--- fooditems/admin.py
+++ fooditems/admin.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from . models import Cart, Item
 
 # Register your models here.
+@admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'category', 'is_veg', 'is_available')
     list_display_links = ('name', 'category')
     list_editable = ('is_available', 'is_veg')
 
 
-admin.site.register(Item, ItemAdmin)
 admin.site.register(Cart)
--- webpages/admin.py
+++ webpages/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 from . models import Slider, Team
 
 # Register your models here.
-admin.site.register(Slider)
-admin.site.register(Team)
+@admin.register(Slider)
+class SliderAdmin(admin.ModelAdmin):
+    list_display = ('headline', 'subtitle', 'button_text')
+
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('name', 'role',)
